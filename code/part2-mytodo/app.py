ab174bf1945fa7f7d1f3011310589e0d51162ecb
coding-to-music <connors.tom@gmail.com>
2022-02-19 05:44:26 +0000

    part2

---
+++ code/part2-mytodo/app.py
@@ -0,0 +1,51 @@
+from chalice import Chalice
+# from chalicelib import db
+from chalicelib.db import InMemoryTodoDB
+
+app = Chalice(app_name='mytodo')
+app.debug = True
+_DB = None
+DEFAULT_USERNAME = 'default'
+
+
+
+
+def get_app_db():
+    global _DB
+    if _DB is None:
+        _DB = InMemoryTodoDB()
+    return _DB
+
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return get_app_db().list_items()
+
+
+# Following the example get_todos() function, add the rest of the required
+# routes here...
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    body = app.current_request.json_body
+    return get_app_db().add_item(
+        description=body['description'],
+        metadata=body.get('metadata'),
+    )
+
+@app.route('/todos/{uid}', methods=['GET'])
+def get_todo(uid):
+    return get_app_db().get_item(uid)
+
+@app.route('/todos/{uid}', methods=['DELETE'])
+def delete_todo(uid):
+    return get_app_db().delete_item(uid)
+
+@app.route('/todos/{uid}', methods=['PUT'])
+def update_todo(uid):
+    body = app.current_request.json_body
+    get_app_db().update_item(
+        uid,
+        description=body.get('description'),
+        state=body.get('state'),
+        metadata=body.get('metadata'))
